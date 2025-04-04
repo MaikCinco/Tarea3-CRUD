4e518dfddd24377436e650f044aa965727ed0a3f
MaikCinco <mgfeliciano5@gmail.com>
2025-04-04 14:25:27 -0400

    Incio proyecto crud

---
+++ src/routes/users.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const router = express.Router();
+const {
+  getUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require('./controllers/usersController');
+
+
+
+router.get('/', getUsers);
+router.get('/:id', getUser);
+router.post('/', createUser);
+router.put('/:id', updateUser);
+router.delete('/:id', deleteUser);
+
+module.exports = router;let users = [];
+let idCounter = 1;
+exports.getUsers = (req, res) => {
+    res.json(users);
+};
+exports.getUser = (req, res) => {
+    const user = users.find(u => u.id === parseInt(req.params.id));
+    user ? res.json(user) : res.status(404).json({ error: 'User not found' });
+};
+exports.createUser = (req, res) => {
+    const newUser = { id: idCounter++, ...req.body };
+    users.push(newUser);
+    res.status(201).json(newUser);
+};
+exports.updateUser = (req, res) => {
+    const index = users.findIndex(u => u.id === parseInt(req.params.id));
+    if (index !== -1) {
+        users[index] = { ...users[index], ...req.body };
+        res.json(users[index]);
+    } else {
+        res.status(404).json({ error: 'User not found' });
+    }
+};
+exports.deleteUser = (req, res) => {
+    const index = users.findIndex(u => u.id === parseInt(req.params.id));
+    if (index !== -1) {
+        const deleted = users.splice(index, 1);
+        res.json(deleted[0]);
+    } else {
+        res.status(404).json({ error: 'User not found' });
+    }
+};
+
